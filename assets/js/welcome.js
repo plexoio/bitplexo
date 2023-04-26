df9bcae79dbb85820104e7a8c296f379bef98ac0
plexoio <plexonft@gmail.com>
2023-04-26 23:29:08 +0200

    refactor: improve js on welcome page titles enter

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,8 +1,24 @@
-let welcomePage = document.getElementById('enter-system');
+// Main Variables
 
-function getMeIn (event) {
+let welcomePage = document.getElementById('enter-system');
+let navWakeUp = document.getElementById('nav-wakeup');
+
+// Captured Event for actions
+
+function getMeIn(event) {
     welcomePage.style.display = 'none';
+    navWakeUp.style.display = 'inherit';
 }
+
+// Add listener to titles and button to enter system
+
+let titleEnter = document.getElementsByClassName('blue-text');
+
+for (let clickTitle = 0; clickTitle < titleEnter.length; clickTitle++) {
+    titleEnter[clickTitle].addEventListener('click', getMeIn);
+}
+
+// Welcome Button listener
 
 let welcomeButton = document.getElementById('enter-page');
 welcomeButton.addEventListener('click', getMeIn);
