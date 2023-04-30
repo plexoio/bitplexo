095d62d61a180dbe804ad16064ed5e42bd50849f
plexoio <plexonft@gmail.com>
2023-04-30 23:42:01 +0200

    refactor: improve contact.html & final fix for switcher .js files

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,11 +1,15 @@
 const openFiat = document.getElementById('fiat-swap'); // Global const extended to other .js
 const cryptoSwitch = document.getElementById('crypto-swap'); // Global const extended to other .js
 
-function innerSwitching (event) {
-    if (openFiat.style.display === "inline-block"){ // If fiat is ON
+function innerSwitching(event) {
+    if (openFiat.style.display === "inline-block") { // If fiat is ON
         openFiat.style.display = "none";
         cryptoSwitch.style.display = "inline-block";
-    } else if (cryptoSwitch.style.display === "inline-block"){ // If crypto is ON
+    } else if (cryptoSwitch.style.display === "inline-block") { // If crypto is ON
+        innerSwitcher.addEventListener('click', (event) => { // Trigger Metamask login
+            event.preventDefault();
+            connect()
+        });
         cryptoSwitch.style.display = "none";
         openFiat.style.display = "inline-block";
     }
@@ -14,12 +18,12 @@ function innerSwitching (event) {
 // Event listener to title 'Switch to Crypto'
 const innerSwitch = document.getElementsByClassName('switch-crypto');
 const innerSwitcher = innerSwitch[0];
-innerSwitcher.addEventListener('click',innerSwitching);
+innerSwitcher.addEventListener('click', innerSwitching);
 
 // Event listener to title 'Switch to Fiat'
 const innerSwitchFiat = document.getElementsByClassName('switch-fiat');
 const innerSwitcherFiat = innerSwitchFiat[0];
-innerSwitcherFiat.addEventListener('click',innerSwitching);
+innerSwitcherFiat.addEventListener('click', innerSwitching);
 
 // for FIAT nav connection
 let toFiat = document.getElementsByClassName('to-fiat');
