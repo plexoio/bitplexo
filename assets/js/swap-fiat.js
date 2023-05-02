581e365b9b4fbfc5d251b4d088f16662b10c96a0
plexoio <plexonft@gmail.com>
2023-05-02 13:56:12 +0200

    progress on swap-fiat.js for currency swapping

--- assets/js/swap-fiat.js
+++ assets/js/swap-fiat.js
@@ -1,21 +1,72 @@
-// Fiat Swap
+// Fiat Swap Global Variables
 let swapFiat = document.getElementById('fiat-swap'); // Whole form
-let selectFromFiat = document.getElementById('fiat-from'); // Select from
-let selectToFiat = document.getElementById('fiat-to'); // Select to
 let enterFiat = document.getElementById('fiat-enter'); // Enter amount
 let receiveFiat = document.getElementById('fiat-receive'); // Receive amount
+let selectFromFiat = document.getElementById('fiat-from'); // Select from
+let selectToFiat = document.getElementById('fiat-to'); // Select to
 
-// Functions
+// Currency pairs object const variable
 
+const currencyPairs = {
+    USD: 'EUR',
+    EUR: 'USD'
+}
+
+// start of PAIRs SELECTION
+
+/**
+ * Listens to the currency selection and execute pairs, 
+ * it follows the const of 'currencyPairs' mapping the key-value pairs
+ * and acting accordingly.
+ */
+
+function currencyPairsFiat (event) {
+    let selectedKeyCurrencyFrom = selectFromFiat.value;
+    let selectedKeyCurrencyto = selectToFiat.value;
+
+    if (currencyPairs[selectedKeyCurrencyFrom]){
+        selectToFiat.value = currencyPairs[selectedKeyCurrencyFrom];
+    } else if (currencyPairs[selectedKeyCurrencyto]) {
+        selectFromFiat.value = currencyPairs[selectedKeyCurrencyto];
+    }
+}
+selectFromFiat.addEventListener('change', currencyPairsFiat);
+selectToFiat.addEventListener('change', currencyPairsFiat);
+
+// end of PAIRs SELECTION
+
+// start of RATE CALCULATION
+
+ /**
+  * RateFiatCalc to run a calculation based on a specific currency selection
+  */
+function rateFiatCalc (event) {
+    let selectedCurrency = selectFromFiat.value;
+    let calcMyNumber = Number(enterFiat.value);
+
+    if (selectedCurrency === 'USD'){
+        usdRateFiatCalc(calcMyNumber);
+    }
+}
+
+/**
+ * Targeted from RateFiatCalc for USD-EUR rate calculation
+ */
+function usdRateFiatCalc (amount) {
+    let receiveMyNumber = amount - (amount * 8.75 / 100);
+    receiveFiat.value = receiveMyNumber;
+}
+
+enterFiat.addEventListener('input', rateFiatCalc);
+
+// end of RATE CALCULATION
+
+// start of BALANCE UPDATE
+
+/**
+ * 
+ */
 function updateNumbersFiat(event){
-
-}
-
-function currencyPairsFiat () {
-
-}
-
-function rateFiatCalc () {
 
 }
 
